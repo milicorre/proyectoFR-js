4ff2e53b94cc3bd2b38f9c9249aaeff8830e2ef7
Milagros Correia <milicorreia91@gmail.com>
2021-09-20 22:17:13 -0300

    agregar id

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -2,6 +2,7 @@ const tienda =[]
 
 class Servicio {
     constructor(servicio) {
+        this.id = servicio.id;
         this.nombre = servicio.nombre;
         this.precio = servicio.precio;
         this.tiempo = servicio.tiempo;
@@ -10,15 +11,15 @@ class Servicio {
     }
 }
 
-const lifting = new Servicio({nombre:"LIFTING DE PESTAÑAS", precio: 1400, tiempo: "Duración del tratamiento: el procedimiento lleva aproximadamente 2h e incluye tintura y dura 4 a 6 semanas, dependiendo del tipo de pelo y cuidados en casa.", zona:"pestañas", descripcion: ' Es un tratamiento que ayuda a mejorar la forma de las pestañas naturales. Las alisa y levanta. Crea la apariencia de pestañas mas largas y rellenas. Un tratamiento que hará que tu mirada se vea mas abierta y juvenil.'})
+const lifting = new Servicio({ id:1, nombre:"LIFTING DE PESTAÑAS", precio: 1400, tiempo: "Duración del tratamiento: el procedimiento lleva aproximadamente 2h e incluye tintura y dura 4 a 6 semanas, dependiendo del tipo de pelo y cuidados en casa.", zona:"pestañas", descripcion: ' Es un tratamiento que ayuda a mejorar la forma de las pestañas naturales. Las alisa y levanta. Crea la apariencia de pestañas mas largas y rellenas. Un tratamiento que hará que tu mirada se vea mas abierta y juvenil.'})
 
-const extensiones = new Servicio({nombre:"EXTENSIONES PELO POR PELO", precio: 2400, tiempo: "Duración del tratamiento: el procedimiento lleva de 2 a 3hs, dependiendo de la cantidad y tipo de pelo del cliente. Y pasados 15/20 días, será necesario realizar un service para retocarlas.", zona:"pestañas", descripcion:'Es un servicio que mejora la estetica de la mirada, puede corregir pequeños defectos segun el diseño que se elija para cada tipo de ojo. Logra aumentar la longuitud, el volumen y la curvatura de las pestañas naturales.'})
+const extensiones = new Servicio({id:2,nombre:"EXTENSIONES PELO POR PELO", precio: 2400, tiempo: "Duración del tratamiento: el procedimiento lleva de 2 a 3hs, dependiendo de la cantidad y tipo de pelo del cliente. Y pasados 15/20 días, será necesario realizar un service para retocarlas.", zona:"pestañas", descripcion:'Es un servicio que mejora la estetica de la mirada, puede corregir pequeños defectos segun el diseño que se elija para cada tipo de ojo. Logra aumentar la longuitud, el volumen y la curvatura de las pestañas naturales.'})
 
-const serviceDeExtensiones = new Servicio({nombre:"SERVICE DE EXTENSIONES", precio: 1700, tiempo: "Duracion del servicio: 2 hs aproximadamente, se realiza cada 15/20 días, dependiendo de la cantidad de extensiones que se conserven.", zona:"pestañas", descripcion:'Se trata del retoque de las extensiones de pestañas pelo por pelo. Un servicio en donde se realiza la limpieza de las pestañas, se remueven las extensiones crecidas y se vuelven a colocar y completar las que fueron cayendo.'})
+const serviceDeExtensiones = new Servicio({id:3,nombre:"SERVICE DE EXTENSIONES", precio: 1700, tiempo: "Duracion del servicio: 2 hs aproximadamente, se realiza cada 15/20 días, dependiendo de la cantidad de extensiones que se conserven.", zona:"pestañas", descripcion:'Se trata del retoque de las extensiones de pestañas pelo por pelo. Un servicio en donde se realiza la limpieza de las pestañas, se remueven las extensiones crecidas y se vuelven a colocar y completar las que fueron cayendo.'})
 
-const extraccion = new Servicio({nombre:"EXTRACCIÓN DE EXTENSIONES", precio: 1000, tiempo: "Duración del servcio: 1 hs aproximadamente", zona:"pestañas", descripcion:'Es un servicio en el cual se extraen las extensiones de pestañas de forma química, sin dañar la pestaña natural.Se utiliza un removedor específico del pegamento, luego de extraerlas se realiza una limpieza de las pestañas.'})
+const extraccion = new Servicio({id:4,nombre:"EXTRACCIÓN DE EXTENSIONES", precio: 1000, tiempo: "Duración del servcio: 1 hs aproximadamente", zona:"pestañas", descripcion:'Es un servicio en el cual se extraen las extensiones de pestañas de forma química, sin dañar la pestaña natural.Se utiliza un removedor específico del pegamento, luego de extraerlas se realiza una limpieza de las pestañas.'})
 
-const nutricion = new Servicio ({nombre:"NUTRICIÓN", precio: 1000, tiempo: "Duración del tratamiento: el procedimiento lleva alrededor de 45 minutos", zona:"pestañas", descripcion:'Es un servicio de botox para nutrir, hidratar y/o recuperar cejas y pestañas. Puede realizarse post tratamiento de lifting o laminado o como tratamiento de recuperación en 4 sesiones, de 1 a 2 veces por semana.'})
+const nutricion = new Servicio ({id:5,nombre:"NUTRICIÓN", precio: 1000, tiempo: "Duración del tratamiento: el procedimiento lleva alrededor de 45 minutos", zona:"pestañas", descripcion:'Es un servicio de botox para nutrir, hidratar y/o recuperar cejas y pestañas. Puede realizarse post tratamiento de lifting o laminado o como tratamiento de recuperación en 4 sesiones, de 1 a 2 veces por semana.'})
 
 tienda.push(lifting, extensiones, serviceDeExtensiones, extraccion, nutricion)
 
